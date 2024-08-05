377bc122684efe5cc69b28bbe801baeaf182947e
Quollveth <quollveth@gmail.com>
2024-08-04 22:16:29 -0300

    finished bloating nvim

--- .config/nvim/lua/my/plugins/lsp/mason.lua
+++ .config/nvim/lua/my/plugins/lsp/mason.lua
@@ -2,20 +2,21 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim"
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lsp = require("mason-lspconfig")
+		local mason_installer = require("mason-tool-installer")
 
 		mason.setup({
 			ui = {
 				icons = {
 					package_installed = "✓",
 					package_pending = "→",
-					package_uninstalled = "✗"
-				}
-			}
+					package_uninstalled = "✗",
+				},
+			},
 		})
 
 		mason_lsp.setup({
@@ -25,5 +26,18 @@ return {
 				"marksman",
 			},
 		})
-	end
+
+		mason_installer.setup({
+			ensure_installed = {
+				---- Formatters
+				"prettier", -- The one and only
+				"stylua", -- Lua formatter
+				"isort", -- Python formatter
+				"black", -- Python is special so it gets two formatters
+
+				---- Linters
+				"luacheck", -- Lua linter
+			},
+		})
+	end,
 }
