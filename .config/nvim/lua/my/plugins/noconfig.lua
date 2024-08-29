b702d4aa3f516ceee9fc098d005f8e20c85544e7
Quollveth <quollveth@gmail.com>
2024-08-29 18:49:33 -0300

    this is a checkpoint in case the break the lsp even more

--- .config/nvim/lua/my/plugins/noconfig.lua
+++ .config/nvim/lua/my/plugins/noconfig.lua
@@ -1,11 +1,11 @@
 -- Plugins that do not need a configuration
 return {
-	{ "nvim-neotest/nvim-nio" },
-	{ "knubie/vim-kitty-navigator" },
-	{ "nvim-lua/plenary.nvim" },
-	{ "tpope/vim-fugitive" },
-	{ "andweeb/presence.nvim" },
-	{ "stevearc/dressing.nvim" },
-	{ "m4xshen/smartcolumn.nvim", opts = { colorcolumn = "120", disabled_filetypes = { "alpha" } } },
-	{ "stevearc/dressing.nvim", event = "VeryLazy" },
+	{ "nvim-neotest/nvim-nio" }, -- dependency for something i forgot what
+	{ "knubie/vim-kitty-navigator" }, -- meow
+	{ "nvim-lua/plenary.nvim" }, -- dependency for something i forgot what
+	{ "tpope/vim-fugitive" }, -- git
+	{ "andweeb/presence.nvim" }, -- discord presence
+	{ "hrsh7th/cmp-cmdline" }, -- autocompletion for the commandline
+	{ "m4xshen/smartcolumn.nvim", opts = { colorcolumn = "120", disabled_filetypes = { "alpha" } } }, -- only show the column when we reach it
+	{ "stevearc/dressing.nvim", event = "VeryLazy" }, -- makes telescope look more nicer
 }
--- .config/nvim/lua/my/plugins/utils/telescope.lua
+++ .config/nvim/lua/my/plugins/utils/telescope.lua
@@ -1,9 +1,9 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
 	-- or                                    branch = '0.1.x',
 	dependencies = {
-		'nvim-lua/plenary.nvim',
+		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
@@ -27,12 +27,14 @@ return {
 			},
 		})
 		-- replaces default ff keybind to only look for git files, pf (project files) is standard search
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Search all file" })
-		vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = "Search git tracked files" })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
-		vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = "Search string under cursor" })
-		vim.keymap.set('n', '<leader>ft', "<cmd>TodoTelescope<CR>", { desc = "Find TODOs" })
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Search all file" })
+		vim.keymap.set("n", "<leader>ff", builtin.git_files, { desc = "Search git tracked files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+		vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Search string under cursor" })
+		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find TODOs" })
 
+		-- extensions
 		telescope.load_extension("fzf")
-	end
+		telescope.load_extension("noice")
+	end,
 }
